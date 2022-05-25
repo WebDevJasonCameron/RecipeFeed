bc640d1f473619881fff438c20e5ef38cb0f29cb
Samuel Arguello <sam.rene12@gmail.com>
2022-05-25 16:40:56 -0500

    first commit

---
+++ src/main/java/com/example/feedme/FeedMeApplication.java
@@ -0,0 +1,13 @@
+package com.example.feedme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeedMeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FeedMeApplication.class, args);
+    }
+
+}
