382e4f3f23f744a36bf187e6e5c66c4434452426
Samuel Arguello <sam.rene12@gmail.com>
2022-05-26 10:12:09 -0500

    set up project layout with directories and security files

---
+++ src/main/java/com/capstone/feedme/SecurityConfiguration.java
@@ -0,0 +1,4 @@
+package com.capstone.feedme;
+
+public class SecurityConfiguration {
+}
--- src/test/java/com/capstone/feedme/FeedMeApplicationTests.java
+++ src/test/java/com/capstone/feedme/FeedMeApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.feedme;
+package com.capstone.feedme;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
