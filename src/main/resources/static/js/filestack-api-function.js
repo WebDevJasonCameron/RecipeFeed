76dcf4c809b106f03dbe8c61544dfde0195ce192
Samuel Arguello <sam.rene12@gmail.com>
2022-06-02 16:37:47 -0500

    added filestack api javascript logic

---
+++ src/main/resources/static/js/filestack-api-function.js
@@ -0,0 +1,17 @@
+"use strict";
+
+const client = filestack.init(FILESTACKAPI);
+let value = null;
+
+const options = {
+    fromSources: ["local_file_system", "url"],
+    accept: ["image/jpeg"],
+    onFileUploadFinished: file => {
+        value = file.originalPath;
+        document.getElementById("imgUrl").value = value;
+    }
+}
+
+document.getElementById("dragDrop").onclick = function (){
+    client.picker(options).open();
+}
