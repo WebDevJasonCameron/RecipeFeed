9078ac8f2eeb23a1743c231bcf681a98a08567cf
Samuel Arguello <sam.rene12@gmail.com>
2022-06-02 12:09:46 -0500

    fixed bug with register controller added model and changed firstname, lastname to username

--- src/main/java/com/capstone/feedme/controllers/UserController.java
+++ src/main/java/com/capstone/feedme/controllers/UserController.java
@@ -1,7 +1,9 @@
 package com.capstone.feedme.controllers;
 
+import com.capstone.feedme.models.User;
 import com.capstone.feedme.repositories.UserRepository;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -22,7 +24,8 @@ public class UserController {
     }
 
     @GetMapping("/register")
-    public String register(){
+    public String register(Model model){
+        model.addAttribute("user", new User());
         return "users/register";
     }
 
