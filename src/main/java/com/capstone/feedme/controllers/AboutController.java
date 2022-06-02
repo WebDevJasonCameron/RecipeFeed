66b983c41ec5b8bae47e744fd909a64ed63b8c83
Jose Lopez <jose.m.lopez747@gmail.com>
2022-06-02 15:20:05 -0500

    updated partial header in htmls and fixed about controller

--- src/main/java/com/capstone/feedme/controllers/AboutController.java
+++ src/main/java/com/capstone/feedme/controllers/AboutController.java
@@ -1,11 +1,14 @@
 package com.capstone.feedme.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+@Controller
 public class AboutController {
 
-    @RequestMapping("/about")
+    @GetMapping("/about")
     public String index(){
-        return "about/index.html";
+        return "about/index";
     }
 }
