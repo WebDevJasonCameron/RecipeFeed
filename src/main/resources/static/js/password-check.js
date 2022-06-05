e627292a3b06037026490321bbd27c000b807051
Samuel Arguello <sam.rene12@gmail.com>
2022-06-05 11:28:40 -0500

    fixed password validation on register form passwords must match to allow form submission, once edited can be submitted. function runs on submit form

--- src/main/resources/static/js/password-check.js
+++ src/main/resources/static/js/password-check.js
@@ -1,13 +1,12 @@
 "use strict";
 
-function validatePassword(){
+function submitForm(event){
     const password = document.getElementById("password").value;
     const confirmPassword = document.getElementById("confirm").value;
-    console.log(password);
-    console.log(confirmPassword);
 
-    if(password !== confirmPassword){
-        alert("Passwords do not match.")
-        document.getElementById("submit").setAttribute("disabled", "disabled")
+    if(!password.match(confirmPassword)){
+        event.preventDefault();
+        alert("Passwords do not match");
     }
+
 }
