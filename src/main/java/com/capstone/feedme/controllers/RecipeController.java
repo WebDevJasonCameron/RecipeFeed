315a21a781474cd01f7fd1ce15582dba44e546f5
Jason Cameron <dev.jasoncameron@gmail.com>
2022-06-06 12:15:31 -0500

    mfeed page has btn to let user find more recipes from api and add to db

--- src/main/java/com/capstone/feedme/controllers/RecipeController.java
+++ src/main/java/com/capstone/feedme/controllers/RecipeController.java
@@ -1,8 +1,10 @@
 package com.capstone.feedme.controllers;
 
 import com.capstone.feedme.models.Category;
+import com.capstone.feedme.models.Ingredient;
 import com.capstone.feedme.models.Recipe;
 import com.capstone.feedme.repositories.CategoryRepository;
+import com.capstone.feedme.repositories.IngredientRepository;
 import com.capstone.feedme.repositories.RecipeRepository;
 import com.capstone.feedme.repositories.UserRepository;
 import org.springframework.stereotype.Controller;
@@ -20,32 +22,121 @@ public class RecipeController {
     private final RecipeRepository recipesDao;
     private final UserRepository usersDao;
     private final CategoryRepository categoryDao;
+    private final IngredientRepository ingredientsDao;
 
     // CON
-    public RecipeController(RecipeRepository recipesDao, UserRepository usersDao, CategoryRepository categoryDao) {
+    public RecipeController(RecipeRepository recipesDao, UserRepository usersDao, CategoryRepository categoryDao, IngredientRepository ingredientsDao) {
         this.recipesDao = recipesDao;
         this.usersDao = usersDao;
         this.categoryDao = categoryDao;
+        this.ingredientsDao = ingredientsDao;
     }
 
     // METH
     @GetMapping
     public String showMainRecipeFeed(Model model){
         List<Recipe> recipes = recipesDao.findAll();
-        Collections.shuffle(recipes);                   // Randomize feed to keep thing fresh
+        Collections.shuffle(recipes);                   // Randomize
 
-        // breakfast
-        List<Recipe> breakfastRecipes = recipesDao.findRecipesByRecipeCategories(categoryDao.findCategoryByType("breakfast"));
-        List<Recipe> morningMealRecipes = recipesDao.findRecipesByRecipeCategories(categoryDao.findCategoryByType("morning meal"));
-        List<Recipe> breakfastRecipesFinal = new ArrayList<>();
-        breakfastRecipesFinal.addAll((breakfastRecipes));
-        breakfastRecipesFinal.addAll((morningMealRecipes));
+        String apiSearchParameter = "";
 
-
-        model.addAttribute("breakfastRecipesFinal", breakfastRecipesFinal);
+        model.addAttribute("apiSearchParameter", apiSearchParameter);
         model.addAttribute("recipes", recipes);
         return "recipes/index";
     }
+
+    @PostMapping
+    public String enterComplexRecipeDetailsIntoDb(@RequestParam(name = "cid") long cid,
+                                                  @RequestParam(name = "title") String title,
+                                                  @RequestParam(name = "image-url") String imageUrl,
+                                                  @RequestParam(name = "summary") String summary,
+                                                  @RequestParam(name = "instructions") String instructions,
+                                                  @RequestParam(name = "ready-in-minutes") String readyInMinutes,
+                                                  @RequestParam(name = "servings") String servings,
+                                                  @RequestParam(name = "source-name") String sourceName,
+                                                  @RequestParam(name = "source-url") String sourceUrl,
+                                                  @RequestParam(name = "vegetarian") boolean vegetarian,
+                                                  @RequestParam(name = "vegan") boolean vegan,
+                                                  @RequestParam(name = "gluten-free") boolean glutenFree,
+                                                  @RequestParam(name = "dairy-free") boolean dairyFree,
+
+                                                  @RequestParam(name = "category-type") String categoryType,
+
+                                                  @RequestParam(name = "ingredient-name") String ingredientName,
+                                                  @RequestParam(name = "ingredient-original") String ingredientOriginal,
+
+                                                  Model model
+    ){
+
+        Recipe recipe;
+        List<Category> categories = new ArrayList<>();
+        List<Ingredient> ingredients = new ArrayList<>();
+
+
+        // IS RECIPE IN DB?
+        if(recipesDao.findRecipeByApiId(cid) != null){
+            recipe = recipesDao.findRecipeByApiId(cid);
+        } else {
+            recipe = new Recipe();
+        }
+
+        // CATEGORIES
+        String[] categoryTypes = categoryType.toLowerCase().split(",");
+
+        // ADD CATS IF NOT IN TABLE
+        for (int i = 0; i < categoryTypes.length; i++) {
+            // if cat type !found, save cat in table
+            if(categoryDao.findCategoryByType(categoryTypes[i]) == null){
+                Category category = new Category(categoryTypes[i]);
+                categoryDao.save(category);
+                categories.add(category);
+            }else{
+                Category category = categoryDao.findCategoryByType(categoryTypes[i]);
+                categories.add(category);
+            }
+        }
+
+        // PLACE DATA IN RECIPE
+        recipe.setApiId(cid);
+        recipe.setTitle(title);
+        recipe.setImgUrl(imageUrl);
+        recipe.setSummary(summary);
+        recipe.setInstruction(instructions);
+        recipe.setReadyInMin(readyInMinutes);
+        recipe.setServingAmount(servings);
+        recipe.setSourceName(sourceName);
+        recipe.setSourceUrl(sourceUrl);
+        recipe.setVegetarian(vegetarian);
+        recipe.setVegan(vegan);
+        recipe.setGlutenFree(glutenFree);
+        recipe.setDairyFree(dairyFree);
+        recipe.setRecipeCategories(categories);
+
+        // INGREDIENTS
+        String[] iNames = ingredientName.split(",");
+        String[] iAmount = ingredientOriginal.split(",");
+
+        for (int i = 0; i < iNames.length; i++) {
+            Ingredient ingredient = new Ingredient( );
+            if( (Long) recipe.getId() == 0) {
+                ingredient.setIngredientName(iNames[i]);
+                ingredient.setIngredientAmount(iAmount[i]);
+                ingredientsDao.save(ingredient);
+                ingredients.add(ingredient);
+            }
+        }
+
+        recipesDao.save(recipe);
+
+        for (int i = 0; i < ingredients.size(); i++) {
+            ingredients.get(i).setRecipe(recipe);
+            ingredientsDao.save(ingredients.get(i));
+        }
+
+        model.addAttribute(recipe);
+        return "/profiles/index";
+    }
+
 
     @GetMapping("/breakfast")
     public String showMainRecipeBreakfastFeed(Model model){
@@ -60,8 +151,11 @@ public class RecipeController {
         recipes.addAll(morningMealRecipes);
         recipes.addAll(brunchRecipes);
 
-        Collections.shuffle(recipes);                   // Randomize feed to keep thing fresh
+        Collections.shuffle(recipes);                   // Randomize
 
+        String apiSearchParameter = "breakfast";
+
+        model.addAttribute("apiSearchParameter", apiSearchParameter);
         model.addAttribute("recipes", recipes);
         return "recipes/index";
     }
@@ -78,8 +172,11 @@ public class RecipeController {
         recipes.addAll(lunchRecipes);
         recipes.addAll(brunchRecipes);
 
-        Collections.shuffle(recipes);                   // Randomize feed to keep thing fresh
+        Collections.shuffle(recipes);               // Randomize
 
+        String apiSearchParameter = "lunch";
+
+        model.addAttribute("apiSearchParameter", apiSearchParameter);
         model.addAttribute("recipes", recipes);
         return "recipes/index";
     }
@@ -90,7 +187,11 @@ public class RecipeController {
         // dinner
         List<Recipe> recipes = recipesDao.findRecipesByRecipeCategories(categoryDao.findCategoryByType("dinner"));
 
-        Collections.shuffle(recipes);                   // Randomize feed to keep thing fresh
+        Collections.shuffle(recipes);                   // Randomize
+
+        String apiSearchParameter = "dinner";
+
+        model.addAttribute("apiSearchParameter", apiSearchParameter);
 
         model.addAttribute("recipes", recipes);
         return "recipes/index";
@@ -102,7 +203,11 @@ public class RecipeController {
         // dessert
         List<Recipe> recipes = recipesDao.findRecipesByRecipeCategories(categoryDao.findCategoryByType("dessert"));
 
-        Collections.shuffle(recipes);                   // Randomize feed to keep thing fresh
+        Collections.shuffle(recipes);                   // Randomize
+
+        String apiSearchParameter = "sweet";
+
+        model.addAttribute("apiSearchParameter", apiSearchParameter);
 
         model.addAttribute("recipes", recipes);
         return "recipes/index";
@@ -119,7 +224,11 @@ public class RecipeController {
         recipes.addAll((mainDishRecipes));
         recipes.addAll((mainCourseRecipes));
 
-        Collections.shuffle(recipes);                   // Randomize feed to keep thing fresh
+        Collections.shuffle(recipes);                   // Randomize
+
+        String apiSearchParameter = "main course";
+
+        model.addAttribute("apiSearchParameter", apiSearchParameter);
 
         model.addAttribute("recipes", recipes);
         return "recipes/index";
@@ -139,7 +248,11 @@ public class RecipeController {
         recipes.addAll(fingerFoodRecipes);
 
 
-        Collections.shuffle(recipes);                   // Randomize feed to keep thing fresh
+        Collections.shuffle(recipes);                   // Randomize
+
+        String apiSearchParameter = "appetizer";
+
+        model.addAttribute("apiSearchParameter", apiSearchParameter);
 
         model.addAttribute("recipes", recipes);
         return "recipes/index";
@@ -160,7 +273,11 @@ public class RecipeController {
         recipes.addAll(saladRecipes);
         recipes.addAll(fingerFoodRecipes);
 
-        Collections.shuffle(recipes);                   // Randomize feed to keep thing fresh
+        Collections.shuffle(recipes);                   // Randomize
+
+        String apiSearchParameter = "";
+
+        model.addAttribute("apiSearchParameter", apiSearchParameter);
 
         model.addAttribute("recipes", recipes);
         return "recipes/index";
@@ -181,7 +298,11 @@ public class RecipeController {
         recipes.addAll(sauceRecipes);
         recipes.addAll(spreadRecipes);
 
-        Collections.shuffle(recipes);                   // Randomize feed to keep thing fresh
+        Collections.shuffle(recipes);                   // Randomize
+
+        String apiSearchParameter = "dip";
+
+        model.addAttribute("apiSearchParameter", apiSearchParameter);
 
         model.addAttribute("recipes", recipes);
         return "recipes/index";
---
+++ src/main/resources/static/js/app.js
@@ -0,0 +1,254 @@
+// VARS, ARRAYS AND LISTS
+let recipeListReturns;
+let recipeDetails;
+
+/**
+ *   PAGE ACTIONS
+ */
+// SEARCH BTN ACTION
+$('#find-more-from-api-btn').on('click', (e) => {
+    e.preventDefault();
+    getSpoonRecipeListByKeyWord($('#search-input').val())
+})
+
+// ACCESS MODAL
+function seeRecipeDetails(id){
+    getSpoonRecipeDetailsByID(id)
+}
+
+
+/**
+ *    API CRUD
+ */
+// GET R LIST
+function getSpoonRecipeListByKeyWord(kw){
+    const apiKey = SPOON_KEY_01;
+    // const apiKey = SPOON_KEY_02;
+    // const apiKey = SPOON_KEY_03;
+
+    const spoonURL = 'https://api.spoonacular.com/recipes/complexSearch?apiKey=' + apiKey + '&query=' + kw + '&offset=10&number=50';
+    const readOption = {
+        method: 'GET',
+    };
+
+    fetch(spoonURL, readOption)
+        .then((res) => res.json())
+        .then((data) => {
+            // console.log(data);
+            recipeListReturns = data;
+        }).then(() => {
+        $('#feed').html(combineCards(recipeListReturns));
+    })
+}
+
+// GET R DETAILS
+function getSpoonRecipeDetailsByID(cid){
+    const apiKey = SPOON_KEY_01;
+    // const apiKey = SPOON_KEY_02;
+    // const apiKey = SPOON_KEY_03;
+
+    const spoonURL = 'https://api.spoonacular.com/recipes/' + cid + '/information?apiKey=' + apiKey;
+    const readOption = {
+        method: 'GET',
+    };
+
+    fetch(spoonURL, readOption)
+        .then((res) => res.json())
+        .then((data) => {
+            console.log(data);
+            recipeDetails = data;
+        })
+        .then(() => {
+            // Loader here
+            $('#recipe-details-modal-content').html(makeModalBody(recipeDetails));
+        });
+}
+
+
+
+
+/**
+ * BUILD BODY
+ */
+// CREATE CARD
+function makeCard(r){
+    return  `
+        <div class="card border-0 mt-4">
+            <div class="row no-gutters">
+                <div class="col-sm-3">
+                    <img src="${r.image}" alt="#" class="card-img-top">
+                </div>
+                <div class="col-sm-9">
+                    <div class="card-body">
+                        <div class="card-title">
+                            ${r.title}
+                        </div>
+                        <div class="h4">
+                             ${r.id}
+                        </div>
+                    </div>
+                    <div class="card-footer">
+                        <button 
+                            onclick="seeRecipeDetails(${r.id})"
+                            class="btn btn-primary" 
+                            type="button" 
+                            data-toggle="modal" 
+                            data-target="#recipe-details-modal"
+                            >
+                                Seed Details
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    `
+}
+
+// COMBINE CARDS
+function combineCards(rL){
+    let output = '';
+    for (let i = 0; i < rL.results.length; i++) {
+        output += makeCard(rL.results[i]);
+    }
+    return output;
+}
+
+// CREATE MODAL BODY
+function makeModalBody(r){
+    return `
+                <div id="recipe-details-modal-content" >
+                    <!--header-->
+                    <div class="modal-header">
+                        <img src="${r.image}" alt="Image data here" class="img-fluid">
+                    </div>
+                    <!--body-->
+                    <div class="modal-body">
+                        <h4>${r.title}</h4>
+                        <ul>
+                            <li>${r.summary}</li>
+                            <li>${r.instructions}</li>
+                            <li>Time: ${r.readyInMinutes}</li> 
+                        </ul>
+                        <h4>Ingredients</h4>
+                        <ol> 
+                        
+                        
+                                ` +  ingredientList(r) + `                   
+                        
+                        
+                        </ol>
+                        <h4>Diet Notes:</h4>
+                        <ul>
+                            <li>vegetarian: ${r.vegetarian}</li>
+                            <li>vegan: ${r.vegan}</li>
+                            <li>gluten free: ${r.glutenFree}</li>
+                            <li>dairy free: ${r.dairyFree}</li>
+                            <li>dish type: ${r.dishTypes}</li>
+                        </ul>
+
+                    </div>
+                    <!--footer-->
+                    <div class="modal-footer">
+                        <div>
+                            <small>Source:
+                                <a href="${r.sourceUrl}" alt="source link">${r.sourceName}</a>
+                            </small>
+                        </div>
+                        <form action="/recipes" method="post">
+                            <div id="recipe-data">
+                                <input type="hidden" name="cid" value="${r.id}">
+                                <input type="hidden" name="title" value="${r.title}">
+                                <input type="hidden" name="image-url" value="${r.image}">
+                                <input type="hidden" name="summary" value="${r.summary.replaceAll('"', '')}">
+                                <input type="hidden" name="instructions" value="${r.instructions.replaceAll('"', '')}">
+                                <input type="hidden" name="ready-in-minutes" value="${r.readyInMinutes}">
+                                <input type="hidden" name="servings" value="${r.servings}">
+                                <input type="hidden" name="source-name" value="${r.sourceName.replaceAll('"', '')}">
+                                <input type="hidden" name="source-url" value="${r.sourceUrl.replaceAll('"', '')}">
+                                <input type="hidden" name="vegetarian" value="${r.vegetarian}">
+                                <input type="hidden" name="vegan" value="${r.vegan}">
+                                <input type="hidden" name="gluten-free" value="${r.glutenFree}">
+                                <input type="hidden" name="dairy-free" value="${r.dairyFree}">
+                                
+                                <div id="categories">
+                                
+                                ` + hiddenCategoryInputList(r) + `
+                                
+                                
+                                </div>
+                                
+                            </div>
+                            <div id="ingredients">      <!--for each-->
+    
+    
+                            ` + hiddenIngredientInputList(r) + `
+    
+    
+                            </div>
+                            <div>
+                                <button 
+                                    class="btn btn-secondary" 
+                                    type="button" 
+                                    data-dismiss="modal">
+                                        Close
+                                </button>                                
+                                <button 
+                                    class="btn btn-primary" 
+                                    type="submit">
+                                        Add to DB
+                                </button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+    `
+}
+
+// CREATE INGREDIENT LIST ITEM
+function ingredientListItem(rI){
+    return `
+        <li>${rI.name}, Amount: ${rI.original}</li>
+    `
+}
+
+// COMBINE INGREDIENTS INTO A LIST
+function ingredientList(r){
+    let output = '';
+    for (let i = 0; i < r.extendedIngredients.length; i++) {
+        output += ingredientListItem(r.extendedIngredients[i]);
+    }
+    return output;
+}
+
+// CREATE HIDDEN INGREDIENT INPUT FOR FORM
+function hiddenIngredientInputs(rI){
+    return `
+                <input type="hidden" name="ingredient-name" value="${rI.name}">
+                <input type="hidden" name="ingredient-original" value="${rI.original}">
+    `
+}
+
+// COMBINE HIDDEN INGREDIENT LIST FOR FORM
+function hiddenIngredientInputList(r){
+    let output = '';
+    for (let i = 0; i < r.extendedIngredients.length; i++) {
+        output += hiddenIngredientInputs(r.extendedIngredients[i]);
+    }
+    return output;
+}
+
+// CREATE HIDDEN CAT INPUT FOR FORM
+function hiddenCategoryInputs(rC){
+    return `
+                <input type="hidden" name="category-type" value="${rC}">
+    `
+}
+
+// COMBINE HIDDEN CAT LIST FOR MORM
+function hiddenCategoryInputList(r) {
+    let output = '';
+    for (let i = 0; i < r.dishTypes.length; i++) {
+        output += hiddenCategoryInputs(r.dishTypes[i]);
+    }
+    return output;
+}
