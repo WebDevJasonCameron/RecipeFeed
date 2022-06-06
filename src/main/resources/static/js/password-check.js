5eb5630d988396d27f0b49f0011e8b92f31c07c7
Samuel Arguello <sam.rene12@gmail.com>
2022-06-06 13:49:02 -0500

    refactored register form to not accept empty fields or duplicate usernames/emails

--- src/main/resources/static/js/password-check.js
+++ src/main/resources/static/js/password-check.js
@@ -1,29 +1,11 @@
 "use strict";
 
 function submitForm(event){
-
-    const username = document.getElementById("username").value;
-    const email = document.getElementById("email").value;
     const password = document.getElementById("password").value;
     const confirmPassword = document.getElementById("confirm").value;
 
     if(!password.match(confirmPassword)){
         event.preventDefault();
         alert("Passwords do not match");
-    }
-
-    if(username === ""){
-        event.preventDefault();
-        alert("Please enter a username")
-    }
-
-    if(email === ""){
-        event.preventDefault();
-        alert("Please enter a email")
-    }
-
-    if(password === ""){
-        event.preventDefault();
-        alert("Please enter a password")
     }
 }
