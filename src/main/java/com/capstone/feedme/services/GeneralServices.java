fb12d960f8603623b768cd457bb6dd9424d43c49
Jason Cameron <dev.jasoncameron@gmail.com>
2022-06-15 20:48:51 -0500

    add append comments to m-feed. you can see them if you click the # comments button under the r-title

--- src/main/java/com/capstone/feedme/services/GeneralServices.java
+++ src/main/java/com/capstone/feedme/services/GeneralServices.java
@@ -13,37 +13,40 @@ import java.util.List;
 @Service
 public class GeneralServices {
 
-    public int getNumberOfRatingsByRecipe(List<Rating> ratings, Recipe recipe){
+    public int getNumberOfRatingsByRecipe(List<Rating> ratings, Recipe recipe) {
         int output = 0;
-        if(ratings == null){
+        if (ratings == null) {
             return 0;
         } else {
             for (int i = 0; i < ratings.size(); i++) {
-                if (ratings.get(i).getRecipe() == null){
+                if (ratings.get(i).getRecipe() == null) {
                     return 0;
-                } else if (ratings.get(i).getRecipe().getId() == recipe.getId()){
-                        output += ratings.get(i).getRating();;
+                } else if (ratings.get(i).getRecipe().getId() == recipe.getId()) {
+                    output += ratings.get(i).getRating();
+                    ;
                 }
             }
         }
         return output;
     }
 
-    public int getNumberOfCommentsByRecipe(List<Comment> comments, Recipe recipe){
+    public int getNumberOfCommentsByRecipe(List<Comment> comments, Recipe recipe) {
 
         List<Comment> outputNumberOfCommentsByRecipe = new ArrayList<>();
 
-        if(comments == null){
+        if (comments == null) {
             return 0;
         } else {
             for (int i = 0; i < comments.size(); i++) {
-                if (comments.get(i).getRecipe() == null){
+                if (comments.get(i).getRecipe() == null) {
                     return 0;
-                } else if (comments.get(i).getRecipe().getId() == recipe.getId()){
+                } else if (comments.get(i).getRecipe().getId() == recipe.getId()) {
                     outputNumberOfCommentsByRecipe.add(comments.get(i));
                 }
             }
         }
         return outputNumberOfCommentsByRecipe.size();
     }
+
+}
 
